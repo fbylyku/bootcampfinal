14971e67b9dbf7e9d1600006ca62bee04e85a941
fbylyku <32739604+fbylyku@users.noreply.github.com>
2019-02-11 23:30:49 -0500

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   db: {
     uri: 'mongodb://fbylyku:password1@ds151137.mlab.com:51137/bootcamp3', //place the URI of your mongo database here.
   },
-  port: process.env.PORT
+  port: 8080
 };
