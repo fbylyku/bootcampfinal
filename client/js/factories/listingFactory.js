0b3402773db4ef5e7ce3b65c2d6da45623e7fab6
fbylyku <32739604+fbylyku@users.noreply.github.com>
2019-02-11 23:32:44 -0500

    Update listingFactory.js

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://https://bootcampss.herokuapp.com/');
+      return $http.get('https://https://bootcampss.herokuapp.com/api/listings');
     },
 
 	create: function(listing) {
-	  return $http.post('https://https://bootcampss.herokuapp.com/', listing);
+	  return $http.post('https://https://bootcampss.herokuapp.com/api/listings', listing);
     },
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-      return $http.delete('https://https://bootcampss.herokuapp.com/' + id);
+      return $http.delete('https://https://bootcampss.herokuapp.com/api/listings' + id);
     }
   };
 
